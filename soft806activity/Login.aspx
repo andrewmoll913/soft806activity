73d24a9cdb1c8ec1138394e95ae2ef35e4241a14
Andrew Moll <mollandrew913@gmail.com>
2019-03-27 11:44:23 +1300

    Add user model.

---
+++ soft806activity/Login.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="Login" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="soft806activity.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="BodyContent" runat="server">
+    <asp:Login ID="UserLogin" runat="server" OnAuthenticate="UserLoginAuthenticate"></asp:Login>
+</asp:Content>
---
+++ soft806activity/Models/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace soft806activity.Models
+{
+    public class User
+    {
+        private string ConnesctionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        private SqlConnection Connection;
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public User(string email)
+        {
+            Email = email;
+        }
+
+        public int Authenticate(string password)
+        {
+            Connection = new SqlConnection(ConnesctionString);
+
+            using (SqlCommand cmd = new SqlCommand("isValidUser"))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@email", Email);
+                cmd.Parameters.AddWithValue("@password", password);
+                cmd.Connection = Connection;
+                Connection.Open();
+                Id = Convert.ToInt32(cmd.ExecuteScalar());
+                Connection.Close();
+            }
+            return Id;
+        }
+    }
+}
